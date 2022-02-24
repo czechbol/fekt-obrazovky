e1248c6493ef82c753ba2e1fb8ba84cb462c259a
czechbol <adamludes@gmail.com>
2022-02-24 01:38:26 +0100

    update

--- __init__.py
+++ __init__.py
@@ -12,74 +12,61 @@ app = Flask(__name__)
 mimetypes.init()
 
 
-def is_image(file):
-    mimestart = mimetypes.guess_type(file)[0]
-
-    if mimestart != None:
-        mimestart = mimestart.split("/")[0]
-
-        if mimestart in ["image"]:
-            return True
-
-    return False
-
-
-def is_video(file):
-    mimestart = mimetypes.guess_type(file)[0]
-
-    if mimestart != None:
-        mimestart = mimestart.split("/")[0]
-
-        if mimestart in ["video"]:
-            return True
-
-    return False
-
-
 @app.route("/")
 def obrazovky():
-    images = []
-    videos = []
+    files = []
     for file in os.listdir(PATH):
-        if is_video(file):
-            videos.append(url_for("static", filename=f"resources/{file}"))
-        elif is_image(file):
-            images.append(url_for("static", filename=f"resources/{file}"))
-    videos.sort()
-    images.sort()
-    if images == [] and videos == []:
+        typ, encoding = mimetypes.guess_type(file)
+        print(typ, encoding)
+
+        if typ is not None and typ.split("/")[0] in ["video", "image"]:
+            files.append(
+                {
+                    "type": typ,
+                    "url": url_for("static", filename=f"resources/{file}"),
+                }
+            )
+
+    files = sorted(files, key=lambda d: d["url"])
+
+    if files == []:
         return render_template("empty.html")
-    elif images == []:
-        return render_template("videos.html", videos=videos)
-    elif videos == []:
-        return render_template("images.html", images=images)
     else:
-        return render_template("both.html", images=images, videos=videos)
+        return render_template(
+            "page.html",
+            files=files,
+        )
 
 
 @app.route("/precise/<folder>")
 def foldered_obrazovky(folder):
-    images = []
-    videos = []
+    files = []
     if os.path.exists(join(PATH, folder)):
         path_str = join(PATH, folder)
     else:
         path_str = PATH
+
     for file in os.listdir(path_str):
-        if is_video(file):
-            videos.append(url_for("static", filename=f"resources/{file}"))
-        elif is_image(file):
-            images.append(url_for("static", filename=f"resources/{file}"))
-    videos.sort()
-    images.sort()
-    if images == [] and videos == []:
+        typ, encoding = mimetypes.guess_type(file)
+        print(typ, encoding)
+
+        if typ is not None and typ.split("/")[0] in ["video", "image"]:
+            files.append(
+                {
+                    "type": typ,
+                    "url": url_for("static", filename=f"resources/{file}"),
+                }
+            )
+
+    files = sorted(files, key=lambda d: d["url"])
+
+    if files == []:
         return render_template("empty.html")
-    elif images == []:
-        return render_template("videos.html", videos=videos)
-    elif videos == []:
-        return render_template("images.html", images=images)
     else:
-        return render_template("both.html", images=images, videos=videos)
+        return render_template(
+            "page.html",
+            files=files,
+        )
 
 
 if __name__ == "__main__":
